18d13880fad9a9a2e4a3d1f704d614cd62c87f1e
cadensanchez28-code <cadensanchez28@gmail.com>
2026-02-18 19:26:08 -0800

    commit 2 fixed

--- startPractice2/src/Person.java
+++ startPractice2/src/Person.java
@@ -1,13 +1,27 @@
+import java.util.Scanner;
 public class Person {
     private String name;
     private int age;
     public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Hello Person Exercise");
         Person person = new Person();
         person.getAge();
         System.out.println("Age is: " + person.getAge());
-        person.setAge(10);
+        System.out.println("Please enter your Age: ");
+        int userage = scanner.nextInt();
+        person.setAge(userage);
         System.out.println("Age is: " + person.getAge());
+        person.getName();
+        scanner.nextLine();
+        System.out.println("Name is: " + person.getName());
+        System.out.println("Please enter your Name: ");
+        String userName = scanner.nextLine();
+        person.setName(userName);
+        System.out.println("Name is: " + person.getName());
+
+
     }
 
     public void Person(String name, int age){
@@ -19,5 +33,11 @@ public class Person {
     }
     public void setAge(int age) {
         this.age = age;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName (String name) {
+        this.name = name;
     }
 }
